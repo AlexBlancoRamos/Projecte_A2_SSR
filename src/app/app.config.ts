46538f6b7d5adcbcabc8c378f9c0ef71520ae45d
MarcRubioo <marc.rubio@institutvidreres.cat>
2024-02-22 12:28:38 +0100

    logout correcto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppComponent } from './app.component';
 import { PlanaPrincipalComponent } from './plana-principal/plana-principal.component';
 import {RouterOutlet} from "@angular/router";
-import {HttpClientModule, HTTP_INTERCEPTORS} from "@angular/common/http";
+import {HttpClientModule, HTTP_INTERCEPTORS, provideHttpClient, withFetch, HttpClient} from "@angular/common/http";
 import {routing} from "./app.routes";
 import {FormsModule} from "@angular/forms";
 import { LoginComponent } from './login/login.component';
@@ -33,7 +33,7 @@ import {TokenInterceptor} from "./token.interceptor";
       useClass: TokenInterceptor,
       multi: true
     },
-    provideClientHydration()
+    provideClientHydration(),
   ],
   bootstrap: [AppComponent]
 })
